e6b8aba5fb19630c5bcd6521139b0a0ae840abfe
Marina Manrique <mmanrique@era7.com>
2015-12-01 17:28:50 +0100

    make cdevel bundle a case object

--- src/main/scala/cdevel.scala
+++ src/main/scala/cdevel.scala
@@ -2,7 +2,7 @@ package ohnosequencesBundles.statika
 
 import ohnosequences.statika._, bundles._, instructions._
 
-abstract class Cdevel extends Bundle() {
+case object Cdevel extends Bundle() {
 
   def instructions: AnyInstructions = cmd("yum")("groupinstall", "-y", "Development Tools")
 }
