51bc222326018bd9a5747b6e2bf12a9a572af5e6
Marina Manrique <mmanrique@era7.com>
2015-12-01 15:58:48 +0100

    fix yum command

--- src/main/scala/cdevel.scala
+++ src/main/scala/cdevel.scala
@@ -4,5 +4,5 @@ import ohnosequences.statika._, bundles._, instructions._
 
 case object cdevel extends Bundle() {
 
-  def instructions: AnyInstructions = cmd("yum")("group", "install", "-y", "Development Tools")
+  def instructions: AnyInstructions = cmd("yum")("groupinstall", "-y", "Development Tools")
 }
